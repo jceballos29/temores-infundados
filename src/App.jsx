55842306b08d32dfc2de9580f6cffcc4f9caba78
Juan Ceballos <jacu29@gmail.com>
2022-11-30 16:53:46 -0500

    add full screen image

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { AudioPlayer, Footer, Header } from './components';
 import {
@@ -11,10 +11,14 @@ import { playlist } from './utils/playlist';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 import gallery from './utils/gallery';
-import temoresInfundados from './assets/img/temores-infundados.png'
+import temoresInfundados from './assets/img/temores-infundados.png';
+import { useState } from 'react';
 
 function App() {
 	const dispatch = useDispatch();
+	const [image, setImage] = useState(null);
+
+	const modalRef = useRef(null)
 
 	useEffect(() => {
 		const audio = playlist.find((audio) => audio.head);
@@ -22,8 +26,8 @@ function App() {
 	}, []);
 
 	const viewImage = (image) => {
-		console.log(image)
-	}
+		setImage(image);
+	};
 
 	return (
 		<BrowserRouter>
@@ -34,16 +38,16 @@ function App() {
 						element={
 							<div className='w-full h-full flex flex-col justify-between p-5'>
 								<Header />
-								<img src={temoresInfundados} alt="" />
+								<img src={temoresInfundados} alt='' />
 							</div>
 						}
 					/>
 					<Route
 						path='/gallery'
 						element={
-							<div className='w-full h-full p-5 overflow-y-auto'>
-								<h2 className='text-xl font-bold'>Galería</h2>
-								<div className='pt-10'>
+							<div className='w-full h-full p-2 overflow-y-auto'>
+								<h2 className='text-3xl font-bold'>Galería</h2>
+								<div className='py-5'>
 									<ResponsiveMasonry
 										columnsCountBreakPoints={{
 											350: 2,
@@ -64,14 +68,23 @@ function App() {
 										</Masonry>
 									</ResponsiveMasonry>
 								</div>
+								{image && (
+									<div ref={modalRef} id='modal' className='fixed w-full h-screen top-0 left-0 bg-black/70 backdrop-blur-sm z-40 flex items-center justify-center overflow-hidden' onClick={(event) => {
+										if(event.target.id === 'modal') {
+											setImage(null)
+										}
+									}}>
+										<img id='modal-image' src={`https://storage.googleapis.com/temores-infundados/medium/${image.title}.jpg`} alt="" className='w-auto max-w-[90%] rounded-md shadow-md' />
+									</div>
+								)}
 							</div>
 						}
 					/>
 					<Route
 						path='/podcast'
 						element={
-							<div className='w-full h-full p-5 overflow-y-auto'>
-								<h2 className='text-xl font-bold'>Podcast</h2>
+							<div className='w-full h-full p-2 overflow-y-auto'>
+								<h2 className='text-3xl font-bold'>Podcast</h2>
 								<div className='pt-5 space-y-1'>
 									{playlist.map((item) => (
 										<div
